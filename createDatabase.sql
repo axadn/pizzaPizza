c3f47a456984ce919728d3cb41dc11e6faa2f334
Adan Marrufo <adanmarrufo@gmail.com>
2018-01-25 23:54:03 -0800

    put database setup in seperate file with .sql extension

---
+++ createDatabase.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS pizzaPizza;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password_digest VARCHAR(60) NOT NULL,
+    session_token VARCHAR NOT NULL UNIQUE,
+    is_admin BOOL NOT NULL
+);
+
+CREATE TABLE sizes IF NOT EXISTS sizes(             
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    name VARCHAR(30) NOT NULL UNIQUE,
+    price DECIMAL(5,2) NOT NULL
+);
+
+CREATE TABLE toppings IF NOT EXISTS toppings(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    name VARCHAR(30) NOT NULL UNIQUE,
+    price DECIMAL(5,2) NOT NULL,
+);
--- db.js
+++ db.js
@@ -5,46 +5,28 @@ const state = {
     pool: null
 };
 
-function seed(pool){
-
+exports.seed = function seed(done){
+    done();
 }
+
+exports.createDatabase = function createDatabase(done){
+    const queries = fs.readFileSync("createDatabase.sql").toString()
+    .replace(/(\r\n|\n|\r)/gm," ")
+    .replace(/\s+/g, ' ');
+    state.pool.query(queries,
+        function (error, results, fields) {
+            done(err);
+        }
+    );
+}
+
 exports.connect = function(done){
     state.pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
         password: 'password',
+        database: 'pizzaPizza'
     });
-    state.pool.query('CREATE DATABASE IF NOT EXISTS pizzaPizza',
-        function (error, results, fields) {
-            if (error) throw error;
-            state.pool.query('CREATE TABLE IF NOT EXISTS users('
-                + 'id INT NOT NULL AUTO_INCREMENT,'
-                + 'PRIMARY KEY(id),'
-                + 'username VARCHAR(30) NOT NULL UNIQUE,'
-                + 'password_digest VARCHAR(60) NOT NULL,'
-                + 'session_token VARCHAR NOT NULL UNIQUE,'
-                + 'is_admin BOOL NOT NULL'
-                +  ')'
-            );
-            state.pool.query('CREATE TABLE sizes IF NOT EXISTS sizes('
-                + 'id INT NOT NULL AUTO_INCREMENT,'
-                + 'PRIMARY KEY(id),'
-                + 'name VARCHAR(30) NOT NULL,'
-                + 'price DECIMAL(5,2) NOT NULL,'
-                + 'CONSTRAINT name_unique UNIQUE(name)'
-                + ')'
-            );
-            state.pool.query('CREATE TABLE toppings IF NOT EXISTS toppings('
-                + 'id INT NOT NULL AUTO_INCREMENT,'
-                + 'PRIMARY KEY(id),'
-                + 'name VARCHAR(30) NOT NULL,'
-                + 'price DECIMAL(5,2) NOT NULL,'
-                + 'CONSTRAINT name_unique UNIQUE(name)'
-                + ')'
-            );
-        }
-    );
-    state.pool.q
     done();
 };
 
