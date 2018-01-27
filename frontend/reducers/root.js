07639bf0c67763ed71e9f20ffa2e5e3feeebdf92
Adan Marrufo <adanmarrufo@gmail.com>
2018-01-26 21:18:10 -0800

    create defaults for reducers

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -1,1 +1,8 @@
 import combineReducers from "redux";
+import entities from "./entities/entities";
+import session from "./session/session";
+
+export default combineReducers({
+    entities,
+    session
+});
